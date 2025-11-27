944ddc0d8ed8eba5382527c6b9ae381f3dadceb1
analyyscool <karljohannesteet@ut.ee>
2025-11-27 20:19:34 +0200

    router

---
+++ router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import MainPage from "../views/mainPage.vue";
+import Signup from "../views/signup.vue";
+
+const routes = [
+  { path: "/", name: "main", component: MainPage },
+  { path: "/signup", name: "signup", component: Signup }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
