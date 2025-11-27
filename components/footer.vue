64d30e8053e796a761edb3dc98a0a0d61deda8a4
analyyscool <karljohannesteet@ut.ee>
2025-11-27 19:18:09 +0200

    components and views

---
+++ components/footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="footer">
+    <p>Bottom text</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
---
+++ components/header.vue
@@ -0,0 +1,50 @@
+<template>
+  <header id="index-header" class="index-header">
+    <div class="left-side">
+      <nav>
+        <router-link to="/" class="nav-link">Home</router-link>
+        <router-link to="/add-post" class="nav-link">Add Post</router-link>
+        <router-link to="/login" class="nav-link">Login</router-link>
+      </nav>
+    </div>
+
+    <div class="right-side">
+      <div class="user-menu">
+        <img
+          src="img/alternative.jpg"
+          alt="User photo"
+          class="profile-image"
+          @click="toggleMenu"
+        />
+
+        <div class="dropdown" :class="{ show: dropdownOpen }">
+          <p class="name"><strong>{{ userName }}</strong></p>
+          <p class="email">{{ userEmail }}</p>
+          <a class="logout" href="#" @click.prevent="logout">Logout</a>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+
+  data() {
+    return {
+      dropdownOpen: false,
+      userName: "Name",          // Later this could come from Vuex
+      userEmail: "name@example.com"
+    };
+  },
+
+  methods: {
+    toggleMenu() {
+      this.dropdownOpen = !this.dropdownOpen;
+    },
+
+  }
+};
+</script>
+
