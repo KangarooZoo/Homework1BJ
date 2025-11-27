f04ad2d36b648bc278176b67274d254dd96480c6
Marten <uhtegi@ut.ee>
2025-11-27 20:59:30 +0200

    Reconfiguring of main.js file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,33 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
+
+/*const App = {
+  components: { PostCard },
+  computed: {
+    posts() { return this.$store.getters.allPosts; }
+  },
+  created() {
+    this.$store.dispatch('fetchPosts');
+  },
+  methods: {
+    resetAll() { this.$store.dispatch('resetAllLikes'); }
+  },
+  template: `
+    <section class="content">
+      <div class="posts-grid">
+        <PostCard v-for="p in posts" :key="p.id" :post-id="p.id" />
+      </div>
+      <button id="reset-likes-btn" type="button" @click="resetAll">
+        Reset all likes
+      </button>
+    </section>
+  `
+};
+
+Vue.createApp(App).use(store).mount('.container.narrow');
+
+*/
