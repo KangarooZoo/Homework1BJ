adc18c9ed5fa81671510c9f0abf4a7b9bc6c9e6d
Jan Alar Alesmaa <jan.alar.alesmaa@ut.ee>
2025-12-14 22:30:21 +0200

    FIx

--- src/views/AddPost.vue
+++ src/views/AddPost.vue
@@ -72,26 +72,3 @@ export default {
   },
 };
 </script>
-<template>
-  <main class="container narrow">
-    <section class="content">
-      <h2>Contacts</h2>
-      <p>Email: example@ut.ee</p>
-      <p>Phone: +372 5555 5555</p>
-    </section>
-  </main>
-</template>
-
-<script>
-import "@/css/index.css";
-
-export default {
-  name: "ContactsView",
-};
-</script>
-
-<style scoped>
-.container {
-  color: black;
-
-}
