e979f2cfd5c7fe5adbd1db03c591ee0d51142b5d
Jan Alar Alesmaa <jan.alar.alesmaa@ut.ee>
2025-12-14 22:33:24 +0200

    fix

---
+++ src/views/Contacts.vue
@@ -0,0 +1,24 @@
+<template>
+    <main class="container narrow">
+      <section class="content">
+        <h2>Contacts</h2>
+        <p>Email: example@ut.ee</p>
+        <p>Phone: +372 5555 5555</p>
+      </section>
+    </main>
+  </template>
+  
+  <script>
+  import "@/css/index.css";
+  
+  export default {
+    name: "ContactsView",
+  };
+  </script>
+  
+  <style scoped>
+  .container {
+    color: black;
+  
+  }
+  </style>
