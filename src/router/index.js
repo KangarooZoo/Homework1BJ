f5eb03f02af4b2a466651582ba6622c5e3ded95d
analyyscool <karljohannesteet@ut.ee>
2025-12-14 22:19:48 +0200

    vueconfig and router index

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,40 @@
-import { createRouter, createWebHistory } from "vue-router";
-import MainPage from "@/views/mainPage.vue";
-import Signup from "@/views/signup.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from '@/views/Login.vue';
+import Signup from '@/views/signup.vue';
+import Home from '@/views/Home.vue';
+import AddPost from '@/views/AddPost.vue';
+import PostView from '@/views/PostView.vue';
+import Contacts from '@/views/Contacts.vue';
 
 const routes = [
-  { path: "/", name: "main", component: MainPage },
-  { path: "/signup", name: "signup", component: Signup }
+  { path: '/', redirect: '/login' },
+  { path: '/login', name: 'Login', component: Login },
+  { path: '/signup', name: 'Signup', component: Signup },
+  { path: '/contacts', name: 'Contacts', component: Contacts },
+  { path: '/home', name: 'Home', component: Home, meta: { requiresAuth: true } },
+  { path: '/addpost', name: 'AddPost', component: AddPost, meta: { requiresAuth: true } },
+  { path: '/post/:id', name: 'PostView', component: PostView, meta: { requiresAuth: true } },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  if (!to.meta.requiresAuth) return next();
+
+  try {
+    const res = await fetch('/auth/authenticate', {
+      method: 'GET',
+      credentials: 'include',
+    });
+    const data = await res.json();
+    if (data.authenticated) next();
+    else next('/login');
+  } catch (e) {
+    next('/login');
+  }
 });
 
 export default router;
